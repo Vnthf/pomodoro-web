38b36bbf092fc90cb0bd1c4dd6de05f24f70e5d4
최민호/NE <minho-choi@nhnent.com>
2016-02-01 19:18:53 +0900

    Revert "initial commit"
    This reverts commit 72e12bc5befce32a0bf1c2e6b53693a12a33d866.

--- app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from 'angular2/core';
-import {TaskList} from './task/taskList.component';
-
-@Component({
-  selector: 'my-app',
-  template: '<task-list></task-list>',
-  directives: [TaskList]
-})
-export class AppComponent { }
